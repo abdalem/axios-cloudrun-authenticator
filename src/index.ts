9a648e37df9b5584a726cacfd734b928e0c66db2
abdalem <abdennour@abbi-studio.fr>
2021-06-02 16:02:00 +0200

    add authenticator method

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from "axios";
+
+const authenticator = async (
+  instance: AxiosInstance,
+  serviceUrl?: string
+): Promise<void> => {
+  if (!serviceUrl) {
+    const originRegex = /^.+?[^\/:](?=[?\/]|$)/i;
+    const regexMatch = originRegex.exec(`${instance.defaults.baseURL}`);
+    if (!regexMatch) return;
+    serviceUrl = regexMatch[0];
+  }
+
+  const metadataServerUrl =
+    "http://metadata/computeMetadata/v1/instance/service-accounts/default/identity?audience=";
+
+  instance.interceptors.request.use(async (req) => {
+    const { data } = await axios.get(metadataServerUrl + serviceUrl, {
+      headers: {
+        "Metadata-Flavor": "Google",
+      },
+    });
+
+    req.headers = {
+      ...req.headers,
+      Authorization: `Bearer ${data}`,
+    };
+    return req;
+  });
+};
+
+export default authenticator;
